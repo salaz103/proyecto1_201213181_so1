e559d5adaf0496cd7cdfee3563f3a0dd95bf2c3e
Luis <salaz103@gmail.com>
2020-12-15 11:45:47 -0600

    Modulo de Ram

--- archivosC/memoria/memo_201213181.c
+++ archivosC/memoria/memo_201213181.c
@@ -6,7 +6,6 @@
 #include<linux/init.h>
 #include<linux/kernel.h>
 #include<linux/fs.h>
-#include<sys/sysinfo.h>
 
 #define BUFSIZE 150;
 
@@ -19,10 +18,20 @@ struct sysinfo inf;
 
 static int escritura_archivo(struct seq_file *archivo, void *v){
     si_meminfo(&inf);
-    long total_memoria= inf.totalram;
-    long libre_memoria= inf.freeram;
-    seq_printf(archivo,"SOPES 1 _ Luis Salazar \n");
-    seq_printf(archivo,"* MEMORIA RAM TOTAL= %d KB \n",total_memoria);
+    long memoriaTotal= (inf.totalram * inf.mem_unit)/(1024*1024);
+    long memoriaLibre= (inf.freeram* inf.mem_unit)/(1024*1024);
+    memoriaLibre= memoriaLibre + 1900; //ESTO SE PUEDE VER CON EL COMANDO free -m en la consola
+    long memoriaUsada= memoriaTotal-memoriaLibre;
+    long porcentajeUso= (100*memoriaUsada)/memoriaTotal;
+
+    seq_printf(archivo,"{");
+    seq_printf(archivo,"\"MTotal\":%lu,", memoriaTotal);
+    seq_printf(archivo,"\"MLibre\":%lu,", memoriaLibre);
+    seq_printf(archivo,"\"MUso\":%lu,", memoriaUsada);
+    seq_printf(archivo,"\"MPUso\":%lu", porcentajeUso);
+    seq_printf(archivo,"}");
+    //seq_printf(archivo,"SOPES 1 _ Luis Salazar \n");
+    //seq_printf(archivo,"* MEMORIA RAM TOTAL= %d KB \n",total_memoria);
     return 0;
 }
 
@@ -41,7 +50,6 @@ static struct file_operations operaciones =
 static int inicio(void){
     proc_create("memo_201213181",0,NULL,&operaciones);
     printk(KERN_INFO "%s","CARGANDO MODULO - 201213181 \n");
-
     return 0;
 }
 
